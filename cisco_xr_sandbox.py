95b75c7b4f90abb7999182ce858b6cd5d3c12b98
nincepython <135132360+nincepython@users.noreply.github.com>
2023-11-07 22:31:34 +0100

    Create cisco_xr_sandbox.py

---
+++ cisco_xr_sandbox.py
@@ -0,0 +1,24 @@
+#!/home/nincepython/.venv/bin/python3
+
+
+import netmiko
+from netmiko import ConnectHandler
+
+device = {
+    'device_type': 'cisco_xr',
+    'ip': 'your_device_ip',
+    'username': 'admin',
+    'password': 'C1sco12345',
+}
+
+# Establish an SSH connection to the device
+connection = ConnectHandler(**device)
+
+# Execute "show ip interface brief" command
+output = connection.send_command("show ip interface brief")
+
+# Print the output
+print(output)
+
+# Close the SSH connection
+connection.disconnect()
