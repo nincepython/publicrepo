67755e807f8af52c0fcd9531b13072bdf2664da0
nincepython <135132360+nincepython@users.noreply.github.com>
2023-11-10 22:28:08 +0100

    Update cisco_xr_sandbox.py

--- cisco_xr_sandbox.py
+++ cisco_xr_sandbox.py
@@ -8,8 +8,8 @@ device = {
     'device_type': 'cisco_xr',
     'host': 'sandbox-iosxr-1.cisco.com',
     # 'ip': 'IP_ADDRESS',
-    #'username': 'admin',
-    'username': input("Enter your username: \n"),
+    'username': 'admin',
+    #'username': input("Enter your username: \n"),
     'password': 'C1sco12345',
 }
 
